9f87f8e18c51d95845ff85bc32938e550fdfc07f
OgbeniHMMD <olajide.a.hammed@gmail.com>
2021-11-14 19:34:05 +0100

    UpdateSplashscreen

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,13 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/images/robot.png"
-      style="width: 200px; height: auto"
-    />
+    <div>
+      <img
+        alt="Quasar logo"
+        src="~assets/images/robot.png"
+        style="width: 200px; height: auto"
+      />
+      <div class="text-center text-primary q-py-md">Work in progress!</div>
+    </div>
   </q-page>
 </template>
 
